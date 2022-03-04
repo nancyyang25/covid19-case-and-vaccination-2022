f024f5fdb72dbdf975181948ca16e738c5a04651
nancyyang25 <98992085+nancyyang25@users.noreply.github.com>
2022-03-04 11:20:09 -0500

    assignment4 final

--- js/scripts.js
+++ js/scripts.js
@@ -3,8 +3,8 @@ mapboxgl.accessToken = 'pk.eyJ1IjoibmFuY3kyMjM1IiwiYSI6ImNremhsenA3dzJibHgyb2t1a
 var map = new mapboxgl.Map({
   container: 'map', // container element id
   style: 'mapbox://styles/mapbox/light-v10',
-  center: [-74.997456, 40.730831], // initial map center in [lon, lat]
-  zoom: 10
+  center: [-74.0060, 40.7128], // initial map center in [lon, lat]
+  zoom: 9.7
 });
 
 
@@ -24,15 +24,15 @@ map.on('load', () => {
         ['linear'],
         ['get', 'coverage-by-modzcta-allages_PERC_FULLY'],
         0,
-        '#f1eef6',
+        '#3bb3c3',
         60,
-        '#bdc9e1',
+        '#669ec4',
         70,
-        '#74a9cf',
+        '#8b88b6',
         80,
-        '#2b8cbe',
+        '#a2719b',
         90,
-        '#045a8d',
+        '#aa5e79',
       ]
     }
   })
