14f683f7fe7e8383009e9fba601c0b71510e30de
nancyyang25 <98992085+nancyyang25@users.noreply.github.com>
2022-03-29 00:46:52 -0400

    Navbar, sidebar, reduced file size

---
+++ README.md
@@ -0,0 +1,4 @@
+# COVID19 Vaccination Rate
+
+## Introduction
+Vaccination rate in each Modified Zip Code Tabulation Areas (MODZCTA.
--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,16 @@
+// mapboxgl access token, used to track my usage of basemaps and other services
 mapboxgl.accessToken = 'pk.eyJ1IjoibmFuY3kyMjM1IiwiYSI6ImNremhsenA3dzJibHgyb2t1aHA5ZzJvYXcifQ.ENgwmLpXLnphzFpbbmo-PQ';
 
+// bounds for a citywide view of New York
+var nycBounds = [[-74.333496,40.469935], [-73.653717,40.932190]]
+
+// initialize the map
 var map = new mapboxgl.Map({
   container: 'map', // container element id
   style: 'mapbox://styles/mapbox/light-v10',
-  center: [-74.0060, 40.7128], // initial map center in [lon, lat]
-  zoom: 9.7
+  bounds: nycBounds, // sets initial bounds instead of center + zoom
+  maxBounds: nycBounds, // sets the max bounds, limited where the user can pan to
+  maxZoom: 9.5 // sets the maximum zoom level
 });
 
 
